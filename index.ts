b6da175e19b80a71f3ddba84caa93eef58e2bf4b
MarkSmersh <marksmersh01@gmail.com>
2025-01-03 13:35:28 +0100

    add generating pages from json

--- index.ts
+++ index.ts
@@ -6,19 +6,25 @@ const SRC_DIR = "src"
 const PAGE = "page"
 const LAYOUT = "layout"
 const SCRIPT = "script"
+const DATA = "data"
 const EXT = "html"
 const ASSETS_DIR = "assets"
 const DATA_DIR = "data"
+const TEMP_DIR = "temp"
 const META_PREFIX = "meta"
+const DATA_PREFIX = "data"
 
 interface Directory {
     dir: string,
     layout: boolean,
     page: boolean,
     script: boolean,
+    data: boolean,
 }
 
 const DIRS: Directory[] = []
+
+const HTML_VARIABLE_REGEX = /\{\{\s*((?:\w+\.?)+)\s*\}\}/gm;
 
 function scanDirectory(directory: string) {
     const dir = fs.readdirSync(directory);
@@ -28,6 +34,7 @@ function scanDirectory(directory: string) {
         layout: false,
         page: false,
         script: false,
+        data: false
     };
 
 
@@ -45,19 +52,11 @@ function scanDirectory(directory: string) {
             continue;
         }
 
-        if (fileName === PAGE) {
-            newDir.page = true;
-            continue;
-        }
-
-        if (fileName === LAYOUT) {
-            newDir.layout = true;
-            continue;
-        }
-
-        if (fileName === SCRIPT) {
-            newDir.script = true;
-            continue;
+        switch (fileName) {
+            case PAGE: newDir.page = true; continue;
+            case LAYOUT: newDir.layout = true; continue;
+            case SCRIPT: newDir.script = true; continue;
+            case DATA: newDir.data = true; continue;
         }
     }
 
@@ -66,18 +65,16 @@ function scanDirectory(directory: string) {
     }
 }
 
-function applyMetaPage(page: string, dir: string)/* : string */ {
-    const htmlMetaRegex = /\{\{\s*((?:\w+\.?)+)\s*\}\}/gm;
-    const metaTags = [...page.matchAll(htmlMetaRegex)];
+
+function applyMetaPage(page: string, dir: string): string {
+    const metaTags = [...page.matchAll(HTML_VARIABLE_REGEX)];
 
     let newPage = page;
 
-    const deepness = dir.split(SRC_DIR).join("").split("/").length - 2
+    const deepness = dir.split(TEMP_DIR).join("").split("/").length - 2
 
     for (let i = 0; i < metaTags.length; i++) {
-        const [prefix, nounUnlowered, ...args] = metaTags[i][1].split(".");
-
-        const noun = nounUnlowered.toLowerCase();
+        const [prefix, noun, ...args] = metaTags[i][1].split(".");
 
         if (prefix !== META_PREFIX) continue;
 
@@ -115,15 +112,66 @@ function applyMetaPage(page: string, dir: string)/* : string */ {
     return newPage;
 }
 
+function makeDataPages(dirs: Directory[]) {
+    const data = dirs.filter((d) => d.data === true)
+
+    for (let i = 0; i < data.length; i++) {
+        const curPage = data[i];
+        const json = JSON.parse(fs.readFileSync(`${curPage.dir}/${DATA}.json`).toString())
+
+        if (json.length <= 0) {
+            continue;
+        }
+
+        for (let j = 0; j < json.length; j++) {
+            let page = fs.readFileSync(`${curPage.dir}/${DATA}.${EXT}`).toString();
+
+            page = applyDataPage(page, json, j);
+
+            const newDir = `${curPage.dir}/${j}`
+            fs.mkdirSync(newDir, { recursive: true })
+
+            fs.writeFileSync(`${newDir}/${PAGE}.${EXT}`, page)
+        }
+    }
+}
+
+function applyDataPage(page: string, json: any, slug: number) {
+    const dataTags = [...page.matchAll(HTML_VARIABLE_REGEX)];
+
+    let newPage = page;
+
+    for (let i = 0; i < dataTags.length; i++) {
+        const [prefix, ...data] = dataTags[i][1].split(".");
+
+        if (prefix !== DATA_PREFIX) continue;
+
+        let dataToAdd = json[slug];
+
+        for (let j = 0; j < data.length; j++) {
+            dataToAdd = dataToAdd[data[j]]
+        }
+
+        console.log(dataToAdd)
+
+        newPage = newPage.replace(
+            dataTags[i][0],
+            dataToAdd
+        )
+    }
+
+    return newPage;
+}
+
 function buildPages(dirs: Directory[]) {
-    const defaultLayout = dirs.find((d) => d.dir === "./" + SRC_DIR)
+    const defaultLayout = dirs.find((d) => d.dir === "./" + TEMP_DIR)
 
     if (!defaultLayout || !defaultLayout?.layout) {
         console.info(`There is no defaut layout in your ${SRC_DIR} directory`)
     }
 
     for (let i = 0; i < dirs.length; i++) {
-        let { dir, page, layout, script } = dirs[i];
+        let { dir, page, layout, script, data } = dirs[i];
 
         if (!page) continue;
 
@@ -166,14 +214,14 @@ function buildPages(dirs: Directory[]) {
             pageToBuild += "</script>\n"
         }
 
-        pageToBuild = applyMetaPage(pageToBuild, dir)
+        pageToBuild = applyMetaPage(pageToBuild, dir);
 
         if (!pageToBuild) {
             console.warn(`${dir}. Page is empty`)
             continue;
         }
 
-        const buildPageDir = `./${BUILD_DIR}${dir.split("./" + SRC_DIR)[1]}`
+        const buildPageDir = `./${BUILD_DIR}${dir.split("./" + TEMP_DIR)[1]}`
 
         if (!fs.existsSync(buildPageDir)) {
             fs.mkdirSync(buildPageDir, { recursive: true })
@@ -188,8 +236,18 @@ function buildPages(dirs: Directory[]) {
 
 function copyFolder(dir: string) {
     fs.cpSync(
-        `./${SRC_DIR}/${dir}`,
+        `./${TEMP_DIR}/${dir}`,
         `./${BUILD_DIR}/${dir}`,
+        {
+            recursive: true
+        }
+    )
+}
+
+function copySrcToTemp() {
+    fs.cpSync(
+        `./${SRC_DIR}`,
+        `./${TEMP_DIR}`,
         {
             recursive: true
         }
@@ -199,11 +257,40 @@ function copyFolder(dir: string) {
 function main() {
     fs.rmSync("./" + BUILD_DIR, { force: true, recursive: true });
 
-    scanDirectory("./" + SRC_DIR);
+    copySrcToTemp()
+
+    scanDirectory("./" + TEMP_DIR);
+
+    makeDataPages(DIRS);
+
+    DIRS.length = 0;
+
+    scanDirectory("./" + TEMP_DIR);
+
     console.log(DIRS);
+
     buildPages(DIRS);
+
     copyFolder(ASSETS_DIR);
+
     copyFolder(DATA_DIR);
+
+    // fs.rmSync("./" + TEMP_DIR, { force: true, recursive: true });
 }
 
 main()
+
+// Our Father, who art in heaven,
+// hallowed be Thy Name,
+// Thy kingdom come,
+// Thy will be done,
+// on earth as it is in heaven.
+// Give us this day our daily bread.
+// And forgive us our sins,
+// as we forgive those
+// who sin against us.
+// And lead us not into temptation,
+// but deliver us from evil.
+// For Thine is the Kingdom,
+// and the power, and the glory,
+// for ever and ever. Amen.
